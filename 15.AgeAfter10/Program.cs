78d5146622b27a9de083fd2f48863bd1e5a7bf4f
Vesko Vasilev <nervenkick@gmail.com>
2015-01-11 20:02:06 +0200

    New Code

--- 15.AgeAfter10/Program.cs
+++ 15.AgeAfter10/Program.cs
@@ -8,9 +8,23 @@ class AgeAfter
 {
     static void Main()
     {
-        Console.Write("Enter your age:");
-        int Age = int.Parse(Console.ReadLine());
-        Console.WriteLine("Your age after 10 years will be:{0}", Age + 10);
+        int yearBorn;
+        while (true)
+        {
+            Console.WriteLine("Please enter the year you were born:");
+            if (int.TryParse(Console.ReadLine(), out yearBorn) && yearBorn > 1900 && yearBorn < DateTime.Now.Year)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Incorect data, please try again!");
+            }
+        }
+
+        int yearsNow = DateTime.Now.Year - yearBorn;
+        int futureAge = yearsNow + 10;
+        Console.WriteLine("Your age now is {0} and you will be {1} years old after 10 years", yearsNow, futureAge);
     }
 }
 
